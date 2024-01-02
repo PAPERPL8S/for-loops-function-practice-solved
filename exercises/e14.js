065ec11785ee4153510c3642350530315256fc83
Andre Ansah <145819692+PAPERPL8S@users.noreply.github.com>
2024-01-01 21:50:24 -0600

    completed e14.js

--- exercises/e14.js
+++ exercises/e14.js
@@ -5,10 +5,42 @@
 // Array example: bankAccounts in /data/data.js
 // getClientsWithWrongBalance(bankAccounts) => [{ name: 'Name1', balance: 32, ... }, { name: 'Name2', balance: 3523, ... }]
 
-export function getClientsWithWrongBalance(array) {
+export function getClientsWithWrongBalance(accounts) {
   // Your code goes here...
 
+const result = [];
+
+  for (const account of accounts) {
+    let sumOfDeposits = 0;
+    for (const deposit of account.deposits) {
+      sumOfDeposits += deposit;
+    }
+
+    let sumOfWithdrawals = 0;
+    if (account.withdrawals) {
+      for (const withdrawal of account.withdrawals) {
+        sumOfWithdrawals += withdrawal;
+      }
+    }
+
+    const expectedBalance = sumOfDeposits - sumOfWithdrawals;
+
+    if (account.balance !== expectedBalance) {
+      result.push(account);
+    }
+
+    if (result.length === 1) {
+      break;
+    }
+  }
+
+  return result;
 }
+
+const bankAccounts = [
+];
+
+console.log(getClientsWithWrongBalance(bankAccounts));
 
 
 
