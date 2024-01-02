c86ce40cd83f01788f7719813a72156170c085b9
Andre Ansah <145819692+PAPERPL8S@users.noreply.github.com>
2024-01-01 23:07:33 -0600

    completed e20.js

--- exercises/e20.js
+++ exercises/e20.js
@@ -5,10 +5,28 @@
 // separateNamesWithAFromRest(["jon", "alice", "andrew", "mark", "jimmy"]) -> [["alice", "andrew", "mark"], ["jon", "jimmy"]]
 // NOTE: You MUST use double/nested FOR loop to solve this exercise. The array.includes() method is NOT allowed.
 
-export function separateNamesWithAFromRest(array) {
+export function separateNamesWithAFromRest(names) {
   // Your code goes here...
+const namesWithA = [];
+  const namesWithoutA = [];
 
+  for (let i = 0; i < names.length; i++) {
+    const currentName = names[i];
+
+    if (currentName.toLowerCase().startsWith('a')) {
+      namesWithA.push(currentName);
+    } else {
+      namesWithoutA.push(currentName);
+    }
+  }
+
+  return [namesWithA, namesWithoutA];
 }
+
+const inputNames = ["jon", "alice", "andrew", "mark", "jimmy", "Oliver", "Evelyn", "Ben"];
+const Result = [['alice', 'andrew'], ['jon', 'mark', 'jimmy']];
+
+const result = separateNamesWithAFromRest(inputNames);
 
 
 // === TEST YOURSELF ===
