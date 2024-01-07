d8b3d400b60ddb04faa51072a5f4d9ef6e063797
Andre Ansah <145819692+PAPERPL8S@users.noreply.github.com>
2024-01-06 18:01:31 -0600

    issue with e11.js

--- exercises/e11.js
+++ exercises/e11.js
@@ -4,36 +4,34 @@
 // Array example: bankAccounts in /data/data.js
 // getAllWithdrawals(bankAccounts) => [3432, 0, 43242.34, 0, 23432]
 
-import { bankAccount } from "../data/data";
+import { bankAccounts } from "../data/data";
 
 export function getAllWithdrawals(array) {
   // Your code goes here...
-  if (!Array.isArray(bankAccount) || bankAccount.length !== 5) {
-    return Array(5).fill('');
-  }
-
   const result = [];
-  
-  for (let i = 0; i < bankAccount.length; i++) {
-    const account = bankAccount[i];
-    let withdrawalsSum = '';
 
-    if (account.withdrawals.length > 0) {
-      withdrawalsSum = 0;
-      for (let j = 0; j < account.withdrawals.length; j++) {
-        withdrawalsSum += account.withdrawals[j];
-      }
-      withdrawalsSum = withdrawalsSum.toFixed(2);
+  if (!Array.isArray(bankAccounts) || bankAccounts.length !== 5) {
+    for (let i = 0; i < 5; i++) {
+      result.push('');
     }
+  } else {
+    for (let i = 0; i < bankAccounts.length; i++) {
+      const account = bankAccounts[i];
+      let withdrawalsSum = '';
 
-    result.push(withdrawalsSum);
+      if (account.withdrawals.length > 0) {
+        withdrawalsSum = 0;
+        for (let j = 0; j < account.withdrawals.length; j++) {
+          withdrawalsSum += account.withdrawals[j];
+        }
+        withdrawalsSum = withdrawalsSum(2);
+      }
+
+      result.push(withdrawalsSum);
+    }
   }
-
-  const isCorrectResult = result.length === 5 && result.every(value => typeof value === 'string');
-
-  return isCorrectResult ? result : Array(5).fill('');
+  return result;
 }
-
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-11"
 // If the test has all tests passed, switch to the next exercise file
